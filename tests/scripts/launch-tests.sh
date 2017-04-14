7d99ca7759e3e3554d2f6ba470170390fc13f5d0
Angelo FILIPPI <angelo.filippi@phelma.grenoble-inp.fr>
2017-04-14 14:07:57 +0200

    modification du script de test

--- tests/scripts/launch-tests.sh
+++ tests/scripts/launch-tests.sh
@@ -7,7 +7,7 @@ fi
 for test_file_in in `ls tests/refs | grep in`
 do
     test_file_out=`echo $test_file_in | sed 's/in/out/'`
-    n=`wc -w tests/refs/$test_file_in | cut -d " " -f 1`
+    n=`wc -w < tests/refs/$test_file_in`
     echo $test_file_in
     bin/compileBST $n tests/refs/$test_file_in > tmp/$test_file_out
 done
